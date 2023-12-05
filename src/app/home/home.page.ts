11c3928ffc1c2029c14ad75ee2aa9847a0334690
n2sy <nidhal.jelassi@fsegt.utm.tn>
2023-12-05 18:01:11 +0100

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,97 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  currentDate;
+  allTasks = [];
+  showAddButton = true;
+ 
 
-  constructor() {}
+  constructor(private http : HttpClient, private alertController : AlertController) {}
 
-}
+  toggleShowAddButton() {
+    this.showAddButton = !this.showAddButton;
+  }
+
+  addNewTask(iValue) {
+    console.log(Date.now);
+    
+    const newTask = {
+      text : iValue,
+      date : new Date(),
+      checked : false
+    };
+    this.http.post('https://ng-tasks-c6b03.firebaseio.com/Tasks.json', newTask).subscribe(
+      {
+        next : (response) => {
+          alert("Task Added Successfully");
+          this.getAllTasks();
+        },
+        error : (err) => {
+          console.log(err);
+          
+        }
+      }
+    )
+
+    this.toggleShowAddButton();
+  }
+
+  getAllTasks() {
+    this.http.get('https://ng-tasks-c6b03.firebaseio.com/Tasks.json').subscribe(
+      {
+        next : (response) => {
+          console.log(response);
+          this.allTasks = [];
+          for (const key in response) {
+            
+            this.allTasks.push({id : key, ...response[key]});
+
+          }
+          console.log(this.allTasks);
+          
+
+          
+        },
+        error : (err) => {
+          console.log(err);
+          
+        }
+      }
+    )
+  }
+
+  async presentAlert(id) {
+    const alert = await this.alertController.create({
+      header: 'Confirmation',
+      message: 'Are you sure to delete this task ?',
+      buttons: ['No', {
+        text : "Yes",
+        handler : () => {
+          this.http.delete(`https://ng-tasks-c6b03.firebaseio.com/Tasks/${id}.json`).subscribe(
+            {
+              next : (response) => {
+                console.log(response);
+                this.getAllTasks();
+                
+                
+              },
+              error :(err) => {
+                console.log(err);
+                
+              }
+            }
+          )
+        }
+      }],
+    });
+
+    await alert.present();
+  }
+
+  ngOnInit() {
+    this.currentDate = new Date();
+    this.getAllTasks();
+
+    
+  }
+  }
