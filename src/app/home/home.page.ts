dac24f7ddb79850648751f983c745e60ec1705ef
n2sy <nidhal.jelassi@fsegt.utm.tn>
2023-12-22 13:01:59 +0100

    add onConvert function

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,25 +8,41 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 })
 export class HomePage {
   allCurrencies = [];
+  msg = '';
 
-  constructor(private http : HttpClient) {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
-      this.http.get('http://apilayer.net/api/live?access_key=cc0c69355ca060873ff4989dad8629f0').subscribe({
-      next: (data) => {
-        //console.log(Object.keys(data['quotes']));
-        for (const curr in data['quotes']) {
-          this.allCurrencies.push(curr.slice(-3));
-        }
-        console.log(this.allCurrencies);
-      },
-      error: (err) => {
-        console.log(err);
-      },
-    });
+    this.http
+      .get(
+        'http://apilayer.net/api/live?access_key=79e10da009bf95d786ad2390b19a81d7'
+      )
+      .subscribe({
+        next: (data) => {
+          //console.log(Object.keys(data['quotes']));
+          for (const curr in data['quotes']) {
+            this.allCurrencies.push(curr.slice(-3));
+          }
+          console.log(this.allCurrencies);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
   }
 
   onConvert(c2) {
     console.log(c2.value);
+    this.http
+      .get(
+        'http://apilayer.net/api/live?access_key=79e10da009bf95d786ad2390b19a81d7'
+      )
+      .subscribe({
+        next: (data) => {
+          console.log(data['quotes']); //data.quotes
+          let req = `USD${c2.value}`;
+          this.msg = `Today, 1 USD worths ${data['quotes'][req]} ${c2.value}`;
+        },
+      });
   }
 }
